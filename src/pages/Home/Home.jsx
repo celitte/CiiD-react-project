efe36a7abbbf43259e916cedd890b370404efe23
celitte <kob221kt@gmail.com>
2023-06-20 21:06:19 +0900

    implement  AddBoard

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,31 +2,38 @@ import "./Home.scss";
 import Layout from "../../components/Layout/Layout";
 import AddBoard from "../../components/AddBoard/AddBoard";
 import Board from "../../components/Board/Board";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const boards = [
-  {
-    id: 1,
-    title: "Weekly tasks",
-  },
-  {
-    id: 2,
-    title: "Private tasks",
-  },
-];
+function Home() {
+  const [boards, setBoards] = useState([]);
 
-const Home = () => {
+  useEffect(() => {
+    const fetchBoards = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/boards");
+        setBoards(response.data);
+      } catch (error) {
+        console.log("error:", error);
+        setError(error.message);
+      }
+    };
+    fetchBoards();
+  }, []);
+
   return (
     <Layout title={"ToDo App"}>
       <div className="page-home">
         <div className="page-home__contents">
-          {boards.map((board, index) => {
-            return <Board key={index} {...board} />;
+          {error && <p>{error}</p>}
+          {boards.map((board) => {
+            return <Board key={board.id} title={board.title} />;
           })}
           <AddBoard />
         </div>
       </div>
     </Layout>
   );
-};
+}
 
 export default Home;
